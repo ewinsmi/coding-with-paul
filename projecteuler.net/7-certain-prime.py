4247e73f0169c4967794d98a2c1857a83a2ed6fd
Paul Ewins <paulewins@Pauls-MacBook-Pro.local>
2020-06-03 14:24:40 +0100

    tidy up code tree

--- projecteuler.net/7-certain-prime.py
+++ projecteuler.net/7-certain-prime.py
@@ -3,7 +3,15 @@ primesList = []
 def isPrimeFactor( number ):
   if number < 2:
     print("not a prime")
-    break
   for i in range(2, number):
-    if number 
+    if number % 2 == 0:
+      break
+    else:
+      primesList.append(number)
 
+isPrimeFactor(9999999999999999999999999999999999999999999999999999999999)
+
+def whichPrimeIsThis(getIndex):
+    return primesList[getIndex + 1]
+
+print(whichPrimeIsThis(10001))
